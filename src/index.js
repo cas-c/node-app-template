e7bac0bbc0924b4e667b8af9065cc4feda7acf27
cassie <hi@cassie.fish>
2023-06-18 15:08:10 -0600

    no more generated js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var otherFile_1 = require("./otherFile");
-console.log("".concat(Date.now()), "node-app-template running!", otherFile_1.importWorking);
--- src/otherFile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.importWorking = void 0;
-// @ts-expect-error unused
-var nonExisting = "This shouldn't be in dist and would throw a compile error";
-exports.importWorking = "Importing and such works as expected";
